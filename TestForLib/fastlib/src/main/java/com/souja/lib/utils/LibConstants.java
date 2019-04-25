264b561e824991c5c0f422028b433ad4795a3939
yangdz <782579195@qq.com>
2019-04-25 15:06:45 +0800

    asfd

--- TestForLib/fastlib/src/main/java/com/souja/lib/utils/LibConstants.java
+++ TestForLib/fastlib/src/main/java/com/souja/lib/utils/LibConstants.java
@@ -6,6 +6,12 @@ public class LibConstants {
     //rx Consumer->key//rx Consumer->key
     public static final int RX_CHOOSE_PHOTO = 12345;//从相册选择图片
     public static final int CROP_IMG = 54321;//剪裁图片
+    public static final int RX_EDIT_IMG = 11111;//编辑图片
+    //编辑图片RequestCode
+    public static int REQ_IMAGE_EDIT = 2018;
+    //预览图片RequestCode
+    public static int REQ_IMAGE_GALLERY = 2019;
+
     public static final int RX_LOGIN_OUTDATE = 66666;//登录过期
 
 }
