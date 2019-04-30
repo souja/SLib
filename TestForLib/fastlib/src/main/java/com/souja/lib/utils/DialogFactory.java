d02a90066b78f080eabd81a8f78e7cc2c0540ea1
yangdezhao <782579195@qq.com>
2019-04-30 13:57:01 +0800

    adf

--- TestForLib/fastlib/src/main/java/com/souja/lib/utils/DialogFactory.java
+++ TestForLib/fastlib/src/main/java/com/souja/lib/utils/DialogFactory.java
@@ -1,5 +1,6 @@
 package com.souja.lib.utils;
 
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.graphics.drawable.Drawable;
@@ -7,6 +8,10 @@ import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.PopupWindow;
+import android.widget.TextView;
+
+import java.util.Calendar;
+import java.util.Locale;
 
 /**
  * Created by Yangdz on 2016/8/11 0011.
@@ -77,7 +82,8 @@ public class DialogFactory {
         return builder.create();
     }
 
-  /*  public static DatePickerDialog getDatePickerDialog(Context context, TextView tvDate) {
+
+    public static DatePickerDialog getDatePickerDialog(Context context, TextView tvDate) {
         Calendar calendar = Calendar.getInstance(Locale.CHINA);
         DatePickerDialog dialog = new DatePickerDialog(context, (view, year, monthOfYear, dayOfMonth) -> {
             String month, day;
@@ -106,101 +112,6 @@ public class DialogFactory {
                 calendar.get(Calendar.MONTH),
                 calendar.get(Calendar.DAY_OF_MONTH));
         return dialog;
-    }*/
-
-//    public interface HourDialogListener {
-//        void onTimeSelect(boolean am, int hour);
-//    }
-
-/*
-    public static AlertDialog getHourPickerDialog(Context context, HourDialogListener listener) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(context);
-        View contentView = LayoutInflater.from(context).inflate(R.layout.dialog_choose_hour, null);
-        ScreenUtil.initScale(contentView);
-        builder.setView(contentView);
-
-        AlertDialog dialog = builder.create();
-        dialog.setCanceledOnTouchOutside(true);
-        RadioGroup rgTime = (RadioGroup) contentView.findViewById(R.id.rg_time);
-        rgTime.check(R.id.rb_am);
-        ListView lvHours = (ListView) contentView.findViewById(R.id.lv_hours);
-        HoursAdapter adapter = new HoursAdapter(context);
-        lvHours.setAdapter(adapter);
-        lvHours.setOnItemClickListener((parent, view, position, id) -> {
-            if (listener != null)
-                listener.onTimeSelect(rgTime.getCheckedRadioButtonId() == R.id.rb_am,
-                        adapter.getItem(position));
-            dialog.dismiss();
-        });
-
-//        Window window = dialog.getWindow();
-//        if (window != null) {
-//            window.setGravity(Gravity.CENTER);
-//        }
-//
-//        WindowManager m = ((Activity) context).getWindowManager();
-//        Display d = m.getDefaultDisplay(); //为获取屏幕宽、高
-//        WindowManager.LayoutParams p = dialog.getWindow().getAttributes(); //获取对话框当前的参数值
-//        p.width = d.getWidth(); //宽度设置为屏幕
-//        dialog.getWindow().setAttributes(p); //设置生效
-        return dialog;
     }
-*/
-
-/*
-    private static class HoursAdapter extends BaseAdapter {
-        private List<Integer> mList;
-        private Context mContext;
-
-        public HoursAdapter(Context context) {
-            mContext = context;
-            mList = new ArrayList<>();
-            for (int i = 1; i < 13; i++) {
-                mList.add(i);
-            }
-        }
-
-        @Override
-        public int getCount() {
-            return mList.size();
-        }
-
-        @Override
-        public Integer getItem(int position) {
-            return mList.get(position);
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return 0;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            HolderHour mHolder;
-            if (convertView == null) {
-                convertView = LayoutInflater.from(mContext).inflate(R.layout.item_hour, null);
-                mHolder = new HolderHour(convertView);
-                convertView.setTag(mHolder);
-            } else mHolder = (HolderHour) convertView.getTag();
-
-            mHolder.tvHour.setText(mList.get(position) + "");
-
-            return convertView;
-        }
-    }
-*/
-
-/*
-    private static class HolderHour {
-        TextView tvHour;
-
-        public HolderHour(View view) {
-            ScreenUtil.initScale(view);
-            tvHour = (TextView) view.findViewById(R.id.tv_hour);
-        }
-    }
-*/
-
 
 }
