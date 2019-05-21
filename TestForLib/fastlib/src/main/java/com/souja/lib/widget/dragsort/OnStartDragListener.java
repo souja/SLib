a411560d4c51eba1e2b65b8292f2bb1131950b92
yangdz <782579195@qq.com>
2019-05-21 18:13:26 +0800

    Revert "asdf"
    This reverts commit 6b6c72fcd345f17f42606a81df6becf18d7c954f.

--- TestForLib/fastlib/src/main/java/com/souja/lib/widget/dragsort/OnStartDragListener.java
+++
@@ -1,7 +0,0 @@
-package com.souja.lib.widget.dragsort;
-
-import android.support.v7.widget.RecyclerView;
-
-public interface OnStartDragListener {
-    void startDrag(RecyclerView.ViewHolder viewHolder);
-}
