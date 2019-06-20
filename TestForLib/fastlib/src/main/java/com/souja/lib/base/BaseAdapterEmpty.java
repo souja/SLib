e793d3eee7ec7e54ad1c43084c36b795a954d693
yangdezhao <782579195@qq.com>
2019-06-20 19:21:22 +0800

    sa123

--- TestForLib/fastlib/src/main/java/com/souja/lib/base/BaseAdapterEmpty.java
+++ TestForLib/fastlib/src/main/java/com/souja/lib/base/BaseAdapterEmpty.java
@@ -4,9 +4,13 @@ import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.souja.lib.R;
+import com.souja.lib.inter.ICommonEmptyCallBack;
 import com.souja.lib.tools.ViewHolderCommon;
+import com.souja.lib.utils.NetWorkUtils;
 
 import java.util.List;
 
@@ -17,6 +21,7 @@ public abstract class BaseAdapterEmpty<T> extends RecyclerView.Adapter<RecyclerV
     public Context mContext;
     public List<T> mList;
     public LayoutInflater mInflater;
+    public final int type_empty = 3;
 
     public BaseAdapterEmpty(Context context, List<T> list) {
         mContext = context;
@@ -29,7 +34,25 @@ public abstract class BaseAdapterEmpty<T> extends RecyclerView.Adapter<RecyclerV
                 R.layout.item_common_nodata, parent, false));
     }
 
-    public void bindEmpty(RecyclerView.ViewHolder holder) {
+    private ICommonEmptyCallBack mCallBack;
+
+    public void setCallBack(ICommonEmptyCallBack callBack) {
+        mCallBack = callBack;
+    }
+
+    public void bindEmpty(RecyclerView.ViewHolder holder, int position) {
+        ImageView ivEmpty = holder.itemView.findViewById(R.id.iv_empty);
+        TextView tvEmpty = holder.itemView.findViewById(R.id.tv_empty);
+        if (!NetWorkUtils.isNetworkAvailable(mContext)) {
+            ivEmpty.setBackgroundResource(R.drawable.ic_no_net);
+            if (mCallBack != null) {
+                tvEmpty.setText(R.string.noNetWork);
+                holder.itemView.setOnClickListener(view -> mCallBack.handleOnCallBack());
+            } else tvEmpty.setText(R.string.noNetWorkB);
+        } else {
+            ivEmpty.setBackgroundResource(R.drawable.ic_empty);
+            tvEmpty.setText("暂无数据");
+        }
     }
 
     public abstract RecyclerView.ViewHolder onCreateView(ViewGroup parent, int viewType);
@@ -38,7 +61,7 @@ public abstract class BaseAdapterEmpty<T> extends RecyclerView.Adapter<RecyclerV
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        if (viewType == 1)
+        if (viewType == type_empty)
             return getEmptyView(parent);
         else
             return onCreateView(parent, viewType);
@@ -46,21 +69,29 @@ public abstract class BaseAdapterEmpty<T> extends RecyclerView.Adapter<RecyclerV
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-        if (mList.size() == 0) bindEmpty(holder);
+        if (mList.size() == 0) bindEmpty(holder, position);
         else {
-            onBindView(holder, position);
+            if (position < mList.size()) {
+                onBindView(holder, position);
+            } else bindLoading(holder);
         }
     }
 
+    public void bindLoading(RecyclerView.ViewHolder holder) {
+
+    }
+
+
     @Override
     public int getItemCount() {
-        return mList.size() + 1;
+        if (mList.size() == 0) return 1;
+        return mList.size();
     }
 
     @Override
     public int getItemViewType(int position) {
         if (mList.size() == 0) {
-            return 1;
+            return type_empty;
         } else {
             return 0;
         }
